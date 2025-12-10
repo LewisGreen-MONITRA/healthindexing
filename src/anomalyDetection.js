d079ac93e1dfa1169e62832bae7d055eebdee933
LewisGreen-MONITRA <lewis.green@monitra.co.uk>
2025-12-10 16:19:41 +0000

    z test in js, for testing

---
+++ src/anomalyDetection.js
@@ -0,0 +1,18 @@
+function mean(arr) {
+    return arr.reduce((sum, v) => sum + v, 0) / arr.length;
+}
+
+function standardDeviation(arr) {
+    const m = mean(arr);
+    const variance = arr.reduce((sum, v) => sum + Math.pow(v - m, 2), 0 ) / arr.length();
+    return Math.sqrt(variance);
+}
+
+function zScore(arr) {
+    const m = mean(arr);
+    const std = standardDeviation(arr);
+    return arr.map(v => (v - m) / sd); 
+}
+
+const data = [10, 20, 30, 40, 50];
+console.log(zScore(data));
