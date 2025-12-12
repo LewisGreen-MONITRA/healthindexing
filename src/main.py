b67e95637567ae1163ed60834e30b3bd17655a13
lewis-green <lewis.green@monitra.co.uk>
2025-12-12 11:57:33 +0000

    tidy up plotting block

--- src/main.py
+++ src/main.py
@@ -3,75 +3,35 @@ Simple anomaly detection method in python.
 Identifies points along time series data that deviates from the mean value 
 to a highly significant degree. 
 """
+from shiny.express import render, ui
+from shared import * 
 
-import numpy as np
-import pandas as pd 
-import math 
-import matplotlib.pyplot as plt
+def server():
+    @render.plot(height=300)
+    def time_series():
 
-from scipy.stats import zscore
-"""
-Seaborn config 
-"""
-import seaborn as sns 
-sns.set_theme(style="whitegrid")
-
-# try and implement without using scipy? 
-def zScore(sample, mu, std): # std deviation can be pulled from the data. 
-    n = len(sample)
-    sample_mu = sum(sample) / n
-    z = (sample_mu - mu) / (std / math.sqrt(n))
-    return z
-
-
+        return 
+    
 def main():
-    df = pd.read_csv("./data.csv")
-    # reading == value rounded to 3 sf
-    # tidy the column names
-    df.columns = df.columns.str.replace(" ", "_")
-    df.columns = df.columns.str.lower()
-
     # filter by sensor
-    sensor = df['sensor_name'].unique()
     selected_sensor = "GT1 W"
-    features = ['value', 'time', 'sensor_name']
-    filtered = df[features]
-    filtered = filtered.set_index('time')
     sensor_filtered_df = filtered[filtered['sensor_name'] == selected_sensor]
-
-    sensor_filtered_df['z_score'] = zscore(sensor_filtered_df['value'])
-    threshold = 2 # standard for cpaturing highly significant outliers
-    outliers = sensor_filtered_df[(sensor_filtered_df['z_score'].abs() > threshold)]
-    # plots trend 
-    sns.lineplot(
-        x=sensor_filtered_df.index,
-        y='value',
-        data=sensor_filtered_df,
-        label='Normal Data',
-        color='blue',
-        linewidth=1.5
-    )          
-    # plot outliers
-    sns.scatterplot(
-        x=outliers.index,
-        y='value',
-        data=outliers,
-        label='Outlier',
-        color='red',
-        s=100, # Adjust size for visibility
-        marker='X' # Use a distinct marker
-    )
-    plt.title(f'Value over time with Z-score Outliers (threshold > {threshold}) for {selected_sensor}')
-    plt.xlabel('Time')
-    plt.ylabel('Value')
-    plt.xticks(rotation=45)
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
+    if len(sensor_filtered_df) >= 30: # high number of samples 
+        print(f'Using Z-score for n > 30')
+        sensor_filtered_df['z_score'] = zscore(sensor_filtered_df['value'])
+        threshold = 3 # standard for cpaturing highly significant outliers, p<0.001 
+        outliers = sensor_filtered_df[(sensor_filtered_df['z_score'].abs() > threshold)]
+        plotResults(sensor_filtered_df, outliers)
+    else: 
+        if len(sensor_filtered_df) != 0: 
+            print(f'Using Modified Z-score for n < 30')
+            threshold = 3.5 #  
+            outliers = sensor_filtered_df[np.abs(modified_zscore(sensor_filtered_df['value']) > threshold)]
+            plotResults(sensor_filtered_df, outliers)
+        else: 
+            print(f'ERROR: No Samples Provided for Sensor {selected_sensor}')
 
     return print(sensor_filtered_df.head(1))
-
 
 if __name__ == "__main__":
     main() 
