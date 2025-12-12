b568ab93982ced2f5f65a4cc0d63b5a9676e09af
lewis-green <lewis.green@monitra.co.uk>
2025-12-12 11:53:35 +0000

    tidy up the plotting block

--- src/shared.py
+++ src/shared.py
@@ -5,6 +5,12 @@ from scipy.stats import *
 import pandas as pd 
 import numpy as np
 
+"""
+Seaborn config 
+"""
+import seaborn as sns 
+import matplotlib.pyplot as plt
+sns.set_theme(style="whitegrid")
 
 app_dir = Path(__file__).parent
 df = pd.read_csv(app_dir / "data.csv")
@@ -28,4 +34,37 @@ def modified_zscore(x):
     mz  = 0.6745 * (x - x_med) / mad
 
     return mz
-    
+
+def plotResults(df, outliers):
+        """
+        Helper function to plot time series health indexing data
+
+        :param df: dataset 
+        :param outliers: outliers detected through z_test 
+        """  
+        # plots trend 
+        sns.lineplot(
+            x=df.index,
+            y='value',
+            data=df,
+            label='Normal Data',
+            color='blue',
+            linewidth=1.5
+        )          
+        # plot outliers
+        sns.scatterplot(
+            x=outliers.index,
+            y='value',
+            data=outliers,
+            label='Outlier',
+            color='red',
+            s=100,
+            marker='o'
+        )
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.xticks(rotation=45)
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
