4fce24b435fc0cbfe5d2ea960fb89348004f88aa
lewis-green <lewis.green@monitra.co.uk>
2025-12-16 11:16:20 +0000

    updates

--- src/main.py
+++ src/main.py
@@ -7,28 +7,31 @@ to a highly significant degree.
 from shared import * 
 def main():
     # filter by sensor
-    selected_sensor = "GT1 W"
+    selected_sensor = "GT1 V"
     sensor_filtered_df = filtered[filtered['sensor_name'] == selected_sensor]
     sensor_filtered_df = sensor_filtered_df[sensor_filtered_df['units'] == "V/cycle"]
     if len(sensor_filtered_df) >= 30: # high number of samples 
         print(f'Using Z-score for n > 30')
         sensor_filtered_df['z_score'] = zscore(sensor_filtered_df['value'])
-        threshold = 2 # standard for cpaturing highly significant outliers, p<0.001 
+        threshold = 3 # standard for cpaturing highly significant outliers, p<0.001 
         outliers = sensor_filtered_df[(sensor_filtered_df['z_score'].abs() > threshold)]
         plotResults(sensor_filtered_df, outliers)
-        a_max = int(0.1 * len(sensor_filtered_df['value']))
-        outlier_index, outlier_value = genESD(sensor_filtered_df['value'], alpha =0.005, max= a_max, return_stats=False)
+        a_max = 0.1 * len(sensor_filtered_df['value'])
+        outlier_index, outlier_value, stats = genESD(sensor_filtered_df['value'], alpha =0.005, max= a_max, return_stats=True)
     else: 
         if len(sensor_filtered_df) != 0: 
             print(f'Using Modified Z-score for n < 30')
-            threshold = 2.5 # standard for capturing highly significant outliers, p<0.001 in modified ztest scenarios 
+            threshold = 3.5 # standard for capturing highly significant outliers, p<0.001 in modified ztest scenarios 
             outliers = sensor_filtered_df[np.abs(modified_zscore(sensor_filtered_df['value']) > threshold)]
             plotResults(sensor_filtered_df, outliers)
             #plotSTL(sensor_filtered_df)
         else: 
             print(f'ERROR: No Samples Provided for Sensor {selected_sensor}')
-
-    return print(outlier_index, outlier_value)
+    print(outlier_index)
+    print(outlier_value)
+    print(stats['R'])
+    print(stats['lambda'])
+    return 
 
 if __name__ == "__main__":
     main() 
